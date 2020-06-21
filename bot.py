44e4c3578bdab89d166e48d194c59a054c3614ff
Jason Liew <jason.97.liew@gmail.com>
2020-06-21 22:23:57 +1000

    Add emote command

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@
 
 import os
 import random
+import json
+import requests
 
 from dotenv import load_dotenv
 from discord.ext import commands
@@ -23,20 +25,16 @@ async def on_ready():
 @bot.command(name='react', help='Sends a reaction emoji to the channel')
 async def react_message(ctx):
 	guild_emojis = ctx.guild.emojis
-	response = str(random.choice(guild_emojis))
+
+	if guild_emojis:
+		response = str(random.choice(guild_emojis))
+	else:
+		response = ':thinking:'
+		
 	await ctx.send(response)
 
 @bot.command(name='roll', help='Simulates a random number generator')
 async def roll(ctx, *args: int):
-	# if len(args) == 0:
-	# 	response = str(random.randint(1, 100))
-	# elif len(args) == 1:
-	# 	response = str(random.randint(1, args[0]))
-	# elif len(args) == 2:
-	# 	response = str(random.randint(args[0], args[1]))
-	# else:
-	# 	response = 'Invalid number of arguments'
-
 	if len(args) == 0:
 		roll_min, roll_max = 1, 100
 	elif len(args) == 1:
@@ -47,7 +45,6 @@ async def roll(ctx, *args: int):
 		raise commands.TooManyArguments
 
 	response = str(random.randint(roll_min, roll_max))
-
 	await ctx.send(response)
 
 @roll.error
@@ -62,6 +59,22 @@ async def roll_error(ctx, error):
 			'```'
 		)
 
+@bot.command(name='emote', help='Send an emote from the FFZ public library')
+async def emote(ctx, name):
+	api_url = 'https://api.frankerfacez.com/v1/emoticons'
+	headers = {'Content-Type': 'application/json'}
+	params = {'q': name, 'sort': 'count-desc', 'high_dpi': 'on', 'page': 1, 'per_page': 20}
+
+	response = requests.get(api_url, params=params, headers=headers)
+
+	if response.status_code == 200:
+		if response.json()['_total'] > 0:
+			emote = response.json()['emoticons'][0]
+			emote_url = (emote['urls'].get('4', False) or emote['urls'].get('2', False) or emote['urls'].get('1', False))
+			await ctx.send('https:' + emote_url)
+	
+	# TODO: Add exception handling
+		
 @bot.command(name='say')
 async def say(ctx):
 	pass
